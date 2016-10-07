61c7f6c68cb463aaeba0989499019dc25d7b9b52
SeimiTerasaki <seimiterasaki@gmail.com>
2016-10-07 12:30:39 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/whoami', function(req, res){
+router.get('/', function(req, res) {
   var result = {'ip-address': req.headers['x-forwarded-for'],
       'language': req.headers['accept-language'].split(",")[0],
       'software': req.headers['user-agent'].split("(")[1].split(")")[0]
-    
   };
   res.json(result);
 });
+
 module.exports = router;
+
 
